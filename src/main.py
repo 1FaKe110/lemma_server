5a7525d43accd21a0485176607ffe074142e1961
1FaKe110 <gabko2016@gmail.com>
2023-11-10 15:38:42 +0300

    debug off

--- src/main.py
+++ src/main.py
@@ -241,7 +241,7 @@ def main():
     with app.app_context():
         db.create_all()
 
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=False)
 
 
 if __name__ == '__main__':
